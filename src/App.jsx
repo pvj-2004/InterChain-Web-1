1a4a68d2b18026819cf5eab50fef169c5cb5edd3
pvj-2004 <prathameshjanugade2004@gmail.com>
2025-01-04 17:33:31 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MemeGenerator from "./Components/MemeGenerator";
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <MemeGenerator />
+      </div>
+    </>
+  )
+}
+
+export default App
